859f733ad8f6e781eae6c66d07a1aa3e9bcf58dd
abigail-g <abigailagracias@gmail.com>
2024-03-15 12:30:18 +0000

    added files for data update and validate

---
+++ data_update.R
@@ -0,0 +1,5 @@
+#Functions to be performed:
+
+# 1. this script will read the csv data 
+# 2. check if files have new records 
+# 3. then append the additional records
---
+++ data_validate.R
@@ -0,0 +1,5 @@
+#Functions to be performed:
+
+# 1. read csv data 
+# 2. checks for data quality and integrity, such as validation of email formats, checking for duplicate entries, and ensuring 
+#    referential integrity
