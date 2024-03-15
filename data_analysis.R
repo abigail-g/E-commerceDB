3db3b4535425c589b61aeaed16ed0d51d06bcb5a
Amani Taha O Elhussein <amaniuc96@gmail.com>
2024-03-15 06:06:19 +0000

    commit to pull

--- data_analysis.R
+++ data_analysis.R
@@ -1,8 +1,17 @@
 #the distribution of counts by order date
+library(ggplot2)
+library(readr)
+
+#rmarkdown::render('E-commerceDB.Rmd')
+
+Order_details <- read_csv("Files/Order_Details.csv",skip = 1)
 
 # Create a histogram of Order Dates
-ggplot(Order_details, aes(x = Order_Date)) +
+plot1 <- ggplot(Order_details, aes(x = Order_Date)) +
   geom_histogram(color = "black", fill = "blue") +
   theme_minimal() +
   labs(title = "Distribution of Order Dates", x = "Order Date", y = "Count")
+
+ggsave(plot = plot1, width = 10, height = 8, dpi = 300,
+       filename =  "Order_Date_Histogram.jpeg")
 
