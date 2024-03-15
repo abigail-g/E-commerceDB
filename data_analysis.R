3d4a3f6bb14e365241d5d2c1c92e60e42023c9e9
asddfghw <40911194@qq.com>
2024-03-15 14:12:59 +0000

    data_analysis

--- data_analysis.R
+++ data_analysis.R
@@ -4,14 +4,32 @@ library(readr)
 
 #rmarkdown::render('E-commerceDB.Rmd')
 
-Order_details <- read_csv("Files/Order_Details.csv",skip = 1)
+library(ggplot2)
+library(RColorBrewer)
 
-# Create a histogram of Order Dates
-plot1 <- ggplot(Order_details, aes(x = Order_Date)) +
-  geom_histogram(color = "black", fill = "blue") +
-  theme_minimal() +
-  labs(title = "Distribution of Order Dates", x = "Order Date", y = "Count")
+con <- dbConnect(RSQLite::SQLite(), "ecommerce.db")
 
-ggsave(plot = plot1, width = 10, height = 8, dpi = 300,
-       filename =  "Order_Date_Histogram.jpeg")
+product_rating_count <- dbGetQuery(con, 'SELECT Product_Rating, COUNT(Product_Rating) AS Product_Rating_Count
+FROM Reviews
+GROUP BY Product_Rating')
+
+library(ggplot2)
+library(RColorBrewer)
+
+product_rating_count$Product_Rating <- as.factor(product_rating_count$Product_Rating)
+# Recreate the chart with a modified background color
+plot1 <- ggplot(product_rating_count, aes(x = Product_Rating, y = Product_Rating_Count, fill = Product_Rating)) +
+  geom_bar(stat = "identity", colour = "black") + # Adding a border
+  scale_fill_brewer(palette = "Spectral") + # Using a colorful palette
+  geom_text(aes(label = Product_Rating_Count), vjust = -0.5, size = 3.5) + # Adding text labels
+  theme_minimal(base_size = 14) + # Adjusting theme and base font size
+  labs(title = "Distribution of Product Ratings",
+       x = "Product Rating",
+       y = "Count of Ratings") +
+  theme(plot.background = element_rect(fill = "white"), # Change the background color of the entire plot
+        panel.background = element_rect(fill = "white"), # Change the background color of the plot panel
+        axis.text.x = element_text(angle = 45, hjust = 1)) # Improve the axis label display
+
+# Save the chart
+ggsave(plot = plot1, filename = "product_rating.jpeg", width = 10, height = 8, dpi = 300)
 
