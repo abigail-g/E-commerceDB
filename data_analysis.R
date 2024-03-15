af09694c4dd6f4f04b7f52eb6aa669a0b1d880eb
asddfghw <40911194@qq.com>
2024-03-15 14:36:36 +0000

    data_analysis

--- data_analysis.R
+++ data_analysis.R
@@ -1,35 +1,34 @@
-#the distribution of counts by order date
-library(ggplot2)
-library(readr)
 
-#rmarkdown::render('E-commerceDB.Rmd')
-
+# Make sure the necessary libraries are loaded
 library(ggplot2)
 library(RColorBrewer)
+library(RSQLite)
 
+# Establish a connection to the database
 con <- dbConnect(RSQLite::SQLite(), "ecommerce.db")
 
-product_rating_count <- dbGetQuery(con, 'SELECT Product_Rating, COUNT(Product_Rating) AS Product_Rating_Count
-FROM Reviews
-GROUP BY Product_Rating')
+# Run the SQL query to get product ratings count by category
+product_rating_count <- dbGetQuery(con, 'SELECT c.Category_Name, a.Product_Rating, COUNT(a.Product_Rating) AS Product_Rating_Count
+FROM Reviews a
+JOIN Products b ON a.Product_ID = b.Product_ID
+JOIN Category c ON b.Category_ID = c.Category_ID
+GROUP BY c.Category_Name, a.Product_Rating')
 
-library(ggplot2)
-library(RColorBrewer)
-
+# Convert Product_Rating to a factor for the ggplot
 product_rating_count$Product_Rating <- as.factor(product_rating_count$Product_Rating)
-# Recreate the chart with a modified background color
-plot1 <- ggplot(product_rating_count, aes(x = Product_Rating, y = Product_Rating_Count, fill = Product_Rating)) +
+
+# Recreate the chart with Category_Name as a fill for coloring the bars
+plot1 <- ggplot(product_rating_count, aes(x = Product_Rating, y = Product_Rating_Count, fill = Category_Name)) +
   geom_bar(stat = "identity", colour = "black") + # Adding a border
-  scale_fill_brewer(palette = "Spectral") + # Using a colorful palette
-  geom_text(aes(label = Product_Rating_Count), vjust = -0.5, size = 3.5) + # Adding text labels
+  scale_fill_brewer(palette = "Spectral", direction = -1) + # Using a colorful palette suitable for discrete values
+  geom_text(aes(label = Product_Rating_Count), position = position_stack(vjust = 0.5), size = 3.5) + # Adding text labels
   theme_minimal(base_size = 14) + # Adjusting theme and base font size
-  labs(title = "Distribution of Product Ratings",
+  labs(title = "Distribution of Product Ratings by Category",
        x = "Product Rating",
        y = "Count of Ratings") +
   theme(plot.background = element_rect(fill = "white"), # Change the background color of the entire plot
         panel.background = element_rect(fill = "white"), # Change the background color of the plot panel
         axis.text.x = element_text(angle = 45, hjust = 1)) # Improve the axis label display
 
-# Save the chart
-ggsave(plot = plot1, filename = "product_rating.jpeg", width = 10, height = 8, dpi = 300)
-
+# Save the chart with a modified background color
+ggsave(plot = plot1, filename = "product_rating_by_category.jpeg", width = 10, height = 8, dpi = 300)
